1987bdf9072ba1e916a9dd1c9b827f60c1bfb51d
German Amaya <vaultcom1@gmail.com>
2021-07-11 10:33:32 -0500

    subiendo

---
+++ prueba2github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  prueba2github
+//
+//  Created by German Amaya on 11/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ prueba2github/prueba2githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  prueba2githubApp.swift
+//  prueba2github
+//
+//  Created by German Amaya on 11/07/21.
+//
+
+import SwiftUI
+
+@main
+struct prueba2githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
